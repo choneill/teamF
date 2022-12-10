8984a7a652f31bec7289d3552cbd44af8cdf45d6
pkud2022 <x22145630@student.ncirl.ie>
2022-12-10 13:26:03 +0000

    some changes
    style.css - tried to css the GoogleMap. Doesn't work, I did in html.
    
    script.js - please help

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+function formSubmission(){
+	
+	var name = document.getElementById("name").value;
+	var phN=document.getElementById("phN").value);
+	var query=document.getElementById("query").value);
+	var textBox=document.getElementById("query").value);
+	
+	//alert("Your inputs are"+name+phN+query+textBox);
+	
+	
+}
+
+function hide(){
+	document.getElementById("contactForm").style.visibility="hidden";
+}
